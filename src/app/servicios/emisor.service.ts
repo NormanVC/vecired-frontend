967d65022141ba192c0ecf44e806707d531c9410
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-07-26 13:03:01 -0400

    gestionar certificados tiene infinite scroll, mensaje cuando se llega al final del infinite scroll compatible con landing page de comunidad sin certificados

--- src/app/servicios/emisor.service.ts
+++ src/app/servicios/emisor.service.ts
@@ -3,23 +3,20 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { EmisorCreados } from '../interfaces/interfaces';
 import { UsuarioService } from './usuario.service';
+
 const url = environment.url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmisorService {
-  paginaEmisor = 0;
-  constructor(private http: HttpClient,
-              private usuarioService: UsuarioService ) {}
+  constructor(private http: HttpClient, private usuarioService: UsuarioService) {}
 
-  getEmisor(){
-    this.paginaEmisor ++;
+  getEmisor(pagina: number) {
     const headers = new HttpHeaders({
       'UToken': this.usuarioService.userToken
     });
 
-    return this.http.get<EmisorCreados>(`${url}/emisor/solicitudes`, { headers });
-
+    return this.http.get<EmisorCreados>(`${url}/emisor/solicitudes?pagina=${pagina}`, { headers });
   }
 }
