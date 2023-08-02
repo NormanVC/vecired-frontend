2b6f14ffed9a5c6fa8beb267d6b1d0b7151b95ca
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-08-01 21:54:54 -0400

    problema al buildear

--- src/app/herramientas/emisor-creado/emisor-creado.component.ts
+++ src/app/herramientas/emisor-creado/emisor-creado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Emisor } from 'src/app/interfaces/interfaces';
+import { AlertasService } from 'src/app/servicios/alertas.service';
+import { EmisorService } from '../../servicios/emisor.service';
 
 @Component({
   selector: 'app-emisor-creado',
@@ -16,11 +18,68 @@ export class EmisorCreadoComponent implements OnInit {
     comunidad: {}
   };
 
-  constructor() { }
+  emisores: Emisor[] = [];
+
+  aceptar: any;
+  rechazar: any;
+
+  constructor( private alertaService: AlertasService,
+                private emisorService: EmisorService) { }
 
   ngOnInit() {
     //console.log(this.emisor);
   }
+
+
+
+  aprobarSolicitud(idSolicitud: string) {
+    //console.log('vamos a aprobar la siguiente solicitud ' + idSolicitud);
+
+    this.alertaService.alertaDecision('¿Desea aceptar a este usuario?').then(
+      respuesta => {
+        this.aceptar = respuesta['data'];
+        if (this.aceptar === true) {
+          this.emisorService.aceptarSolicitud(idSolicitud).subscribe(
+            (respuesta) => {
+             // console.log(respuesta);
+              
+              window.location.reload();
+            },
+            (error) => {
+              console.error('Error al aprobar la solicitud: ', error);
+            }
+          );
+        } else {
+          this.alertaService.presentToast('Operación cancelada');
+        }
+      }
+    );
+  }
+
+  rechazarSolicitud(idSolicitud: string) {
+    //console.log('vamos a rechazar la siguiente solicitud ' + idSolicitud);
+
+    this.alertaService.alertaDecision('¿Desea rechazar a este usuario?').then(
+      respuesta => {
+        this.aceptar = respuesta['data'];
+        if (this.aceptar === true) {
+          this.emisorService.rechazarSolicitud(idSolicitud).subscribe(
+            (respuesta) => {
+             // console.log(respuesta);
+
+              window.location.reload();
+            },
+            (error) => {
+              console.error('Error al rechazar la solicitud: ', error);
+            }
+          );
+        } else {
+          this.alertaService.presentToast('Operación cancelada');
+        }
+      }
+    );
+  }
+
 
 }
 
