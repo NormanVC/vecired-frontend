0b096defd08d56dfcff448e765d05ff7faeac4f1
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-10-18 11:03:18 -0300

    cambios visuales en login

--- src/app/pages/verificador/verificador.page.ts
+++ src/app/pages/verificador/verificador.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertasService } from 'src/app/servicios/alertas.service';
 
 @Component({
   selector: 'app-verificador',
@@ -7,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerificadorPage implements OnInit {
   solicitud: string;
-  constructor() { }
+  constructor(private alertasService: AlertasService) { }
 
   ngOnInit() {
   }
 
   verificar(){
     console.log(this.solicitud);
+
+
+    //Toast, to do configurarla por si el input viene vacio o es menor a 24
+    this.alertasService.presentToast('ALL THE BOYS  SAY WOW WOW ');
   }
 
 }
