04e88fe5309517d5561c9ed9f2ffaffc1450356e
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-11-29 14:14:42 -0300

    verificador

--- src/app/pages/validate/validate.page.ts
+++ src/app/pages/validate/validate.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { EmisorService } from 'src/app/servicios/emisor.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Comunidad, Emisor, Usuario } from 'src/app/interfaces/interfaces';
 
 @Component({
@@ -14,9 +14,14 @@ export class ValidatePage implements OnInit {
   usuario: Usuario={};
   comunidad: Comunidad={};
   loading: boolean = true;
+
+  nombreParseado: any;
+  apellidoParseado: any;
+
  
   constructor( private emisorService: EmisorService,
-              private ruta: ActivatedRoute
+              private ruta: ActivatedRoute,
+              private router: Router
               ) { }
 
   ngOnInit() {
@@ -31,17 +36,34 @@ export class ValidatePage implements OnInit {
       this.emisorService.getID(idSolicitud).subscribe(
         (respuesta: any) => {
           this.emisor = respuesta.resultado;
-          this.loading = false; // Marcamos que la carga ha terminado
+          this.loading = false;
+
+          const partesNombre = this.parsearNombre(this.emisor.usuario.nombre);
+          this.nombreParseado = partesNombre[0];
+          this.apellidoParseado = partesNombre[1];
+
+
         },
         (error) => {
           console.error('Error al obtener datos:', error);
-          this.loading = false; // También marcamos que la carga ha terminado en caso de error
+          this.loading = false;
         }
       );
     });
   
   }
 
+  parsearNombre(nombreCompleto: string): [string, string] {
+    const partes = this.emisor.usuario.nombre.split(' ');
+    const nombre = partes.length > 0 ? partes[0] : nombreCompleto;
+    const apellido = partes.length > 1 ? partes.slice(1).join(' ') : '';
+    return [nombre, apellido];
+  }
+
+
+ aCasa(){
+  this.router.navigateByUrl('/login');
+ }
 
 
 }
